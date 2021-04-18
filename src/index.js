5d41e7b4cb8eca2a8300280966e47d9e1e8ffa3f
xianyu <yuanfan.254745812@qq.com>
2021-04-18 23:54:05 +0800

    优化

--- src/index.js
+++ src/index.js
@@ -1,91 +1,118 @@
-
 import React from "./mini-react/react";
 import ReactDOM from "./mini-react/react-dom";
 
 class Count extends React.Component {
-  state = {count:1}
-  render(){
-    return <div>
+  state = { count: 1 };
+  add=() => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+  render() {
+    return (
+      <div>
         <p>{this.state.count}</p>
-        <button id="btn" onClick={()=>{
-            this.setState({
-              count: this.state.count+1
-            })
-        }}>按钮</button>
-    </div>
+        <button
+          id="btn"
+          onClick={this.add}
+        >
+          按钮
+        </button>
+      </div>
+    );
   }
 }
 class Li extends React.Component {
-  render(){
-    const {data,remove} = this.props;
-    const {id,title} = data;
-    return <li>{title} --- <button onClick={()=>{
-      remove(id);
-    }}>删除</button></li>
+  
+  remove=() => {
+    this.props.remove(this.props.data.id);
+  }
+  render() {
+    const { data} = this.props;
+    const { title } = data;
+    return (
+      <li>
+        {title} ---{" "}
+        <button
+          onClick={this.remove}
+        >
+          删除
+        </button>
+      </li>
+    );
   }
 }
 class Todo extends React.Component {
-  render(){
-    const {index,todo,addTodo,remove} = this.props;
-    return <div 
-      className={"todo-"+index}
-      style={{
-        width: 200 + (index*20) + "px",
-        border: "2px solid #000"
-      }}
-    >
-      <h2>todo - <button onClick={addTodo}>添加</button></h2>
-      {todo.map(item=>{
-          return <Li data={item} key={item.id} remove={remove} />
-      })}  
-    </div>
+  render() {
+    const { index, todo, addTodo, remove } = this.props;
+    return (
+      <div
+        className={"todo-" + index}
+        style={{
+          width: 200 + index * 20 + "px",
+          border: "2px solid #000",
+        }}
+      >
+        <h2>
+          todo - <button onClick={addTodo}>添加</button>
+        </h2>
+        <ul>
+          {todo.map((item) => {
+            return <Li data={item} key={item.id} remove={remove} />;
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 class App extends React.Component {
   state = {
     index: 1,
-    data:[
+    data: [
       {
         id: 0,
-        title: "这是todo-0"
-      },{
+        title: "这是todo-0",
+      },
+      {
         id: 1,
-        title: "这是todo-1"
-      }
-    ]
-  }
-  addTodo=()=>{
-    const {index,data} = this.state;
+        title: "这是todo-1",
+      },
+    ],
+  };
+  addTodo = () => {
+    const { index, data } = this.state;
     this.setState({
       index: index + 1,
-      data: [...data,{
-        id: index + 1,
-        title: "这是todo-"+(index+1)
-      }]
-    })
-  }
-  remove=(id)=>{
-    const {data} = this.state;
+      data: [
+        ...data,
+        {
+          id: index + 1,
+          title: "这是todo-" + (index + 1),
+        },
+      ],
+    });
+  };
+  remove = (id) => {
+    const { data } = this.state;
     this.setState({
-      data: data.filter(item=>item.id!==id)
-    })
-  }
-  render(){
-    const {index,data} = this.state;
-    return <div>
+      data: data.filter((item) => item.id !== id),
+    });
+  };
+  render() {
+    const { index, data } = this.state;
+    return (
+      <div>
         <Count />
         <hr />
-        <Todo 
-          addTodo = {this.addTodo}
+        <Todo
+          addTodo={this.addTodo}
           index={index}
           todo={data}
-          remove = {this.remove}
-        />       
-    </div>
+          remove={this.remove}
+        />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(
-  <App/>, 
-  document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
